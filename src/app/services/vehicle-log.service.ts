c63ec9a188d850c4d88c9455f54fb941cba48b78
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2025-08-08 01:42:18 +0530

    Mapped frontend params to backend query ident before API call

--- src/app/services/vehicle-log.service.ts
+++ src/app/services/vehicle-log.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { VehicleLogData } from '../models/vehicle-log.model';
+import {SearchFilters, VehicleLogData} from '../models/vehicle-log.model';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -9,12 +9,13 @@ export class VehicleLogService {
 
   constructor(private http: HttpClient) {}
 
-  getLogs(filters: any): Observable<VehicleLogData[]> {
+  getLogs(filters: SearchFilters): Observable<VehicleLogData[]> {
     let params = new HttpParams();
-    if (filters.vehicleId) params = params.set('vehicleId', filters.vehicleId);
-    if (filters.errorCode) params = params.set('errorCode', filters.errorCode);
-    if (filters.startDate) params = params.set('startDate', filters.startDate);
-    if (filters.endDate) params = params.set('endDate', filters.endDate);
+    if (filters.vehicleId) params = params.set('vehicle', filters.vehicleId);
+    if (filters.code) params = params.set('code', filters.code);
+    if (filters.startDate) params = params.set('from', filters.startDate);
+    if (filters.endDate) params = params.set('to', filters.endDate);
+    console.log(params.toString());
 
     return this.http.get<VehicleLogData[]>(this.apiUrl, { params });
   }
