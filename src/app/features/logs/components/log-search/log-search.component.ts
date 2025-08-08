448c429c6d7e2097c1229c27d50e0eaf2424a956
nobodyn0se <48710616+nobodyn0se@users.noreply.github.com>
2025-08-08 16:31:02 +0530

    Added validator to form data

--- src/app/features/logs/components/log-search/log-search.component.ts
+++ src/app/features/logs/components/log-search/log-search.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import {FormGroup, ReactiveFormsModule, FormControl} from '@angular/forms';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
 
@@ -23,10 +23,50 @@ export class LogSearchComponent {
     vehicleId: new FormControl<string>(''),
     code: new FormControl<string>(''),
     startDate: new FormControl<string | null>(null),
-    endDate: new FormControl<string | null>(null)
-  });
+    endDate: new FormControl<string | null>(null),
+  }, [this.generalValidator, this.dateRangeValidator]);
+
+  get minEndDate(): string {
+    return this.form.get('startDate')?.value || '';
+  }
+
+  generalValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const formGroup = control as FormGroup; // Cast the control to FormGroup
+      const vehicleId = formGroup.get('vehicleId')?.value;
+      const code = formGroup.get('code')?.value;
+      const startDate = formGroup.get('startDate')?.value;
+      const endDate = formGroup.get('endDate')?.value;
+
+      if (!vehicleId && !code && !(startDate && endDate)) {
+        return { atLeastOneRequired: true };
+      }
+      return null;
+    };
+  }
+
+  // Date range validation - only triggered when dates are provided
+  dateRangeValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const formGroup = control as FormGroup; // Cast the control to FormGroup
+      const startDate = formGroup.get('startDate')?.value;
+      const endDate = formGroup.get('endDate')?.value;
+
+      // Only validate date range if startDate is provided
+      if (startDate || endDate) {
+        if (!startDate || !endDate) {
+          return { incompleteDateRange: true };
+        }
+      }
+
+      return null;
+    };
+  }
 
   onSubmit() {
+    if(this.form.invalid) {
+      return;
+    }
     const raw = this.form.value;
 
     // Clean nulls (or empty strings) to undefined
@@ -47,6 +87,15 @@ export class LogSearchComponent {
   onReset() {
     this.form.reset();
     this.search.emit({});
+  }
+
+  get isFormValid() {
+    const vehicleId = this.form.get('vehicleId')?.value;
+    const code = this.form.get('code')?.value;
+    const startDate = this.form.get('startDate')?.value;
+    const endDate = this.form.get('endDate')?.value;
+
+    return vehicleId || code || (startDate && endDate);
   }
 }
 
